79083a6b187dd3c4d50d229b1bfe7117c2cc87b8
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-24 02:57:17 +0000

    some errors fixed

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ document.getElementById('mybtn').addEventListener('click', calculatearea)
 function calculatearea () {
   let D = 0
   let R = 0
-  let PI = 3.14159265359
+  const PI = 3.14159265359
   let A = 0
   let C = 0
-  
-  D = document.getElementById('D').value
+  D = document.getElementById('d').value
   R = parseFloat(D) / 2
   A = PI * (R * R)
   alert(A)
